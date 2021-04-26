b5b8374b4a4c7bd6bcdbe397026ca52ef9ff3963
willGraham01 <wg270@bath.ac.uk>
2021-04-26 15:30:52 +0100

    Code reorganisation for the Density of States plots, and associated edits to diagrams and outputs, after the Green's Identity fix

--- QG_Code/DoS_CrossInPlane_ThickVertex.py
+++ QG_Code/DoS_CrossInPlane_ThickVertex.py
@@ -8,13 +8,13 @@ Created on Tue Jul 28 08:31:12 2020
 Script and functions to numerically compute the (Integrated) Density of States for the TFR thick-vertex cross-in-plane setup, see section 2.2 of the TFR.
 
 Equation (2.7) in section 2.2 gives that eigenvalues w are those which solve the following equation:
-	-(alpha*w/4)sin(w) + cos(w) = cos((theta1+theta2)/2)cos((theta1-theta2)/2)
+	(alpha*w/4)sin(w) + cos(w) = cos((theta1+theta2)/2)cos((theta1-theta2)/2)
 This will necesitate the use of fsolve to determine the eigenvalues which a given theta (quasimomentum) corresponds to.
 Propositions 2.2 and 2.3 in section 2.2 also demonstrate that each "band" I_n is contained in the interval [(n-1)pi, n*pi], so we can utilise this knowledge to compute the Integrated DoS in over each band sequentally. Furthermore, we know that the right-endpoint of each band is precisely n*pi.
 
 A recommended call to Visual_FullDoS is along the lines of
 f = Visual_FullDoS(alpha, lastBand=n, nPts=100, dx=1e-1)
-where n is the index of the final spectral band that you want to be included in the plot. 
+where n is the index of the final spectral band that you want to be included in the plot.
 This call is relatively fast (due to low nPts) and also produces relatively good quality for bands up to n=5.
 More detail on the choice of parameters can be found in the function comments themselves.
 
@@ -44,8 +44,8 @@ def LHS(w, alpha):
 	Outputs:
 		LHSVals: (n,) float numpy array, evaluation of the LHS
 	'''
-	
-	LHSVals = -(alpha/4) * w * sin(w) + cos(w) #default np array ops are element-wise
+
+	LHSVals = -(alpha/4.) * w * sin(w) + cos(w) #default np array ops are element-wise
 	return LHSVals
 
 def LHSDeriv(w, alpha):
@@ -57,7 +57,7 @@ def LHSDeriv(w, alpha):
 	Outputs:
 		LHSDerivVals: (n,) float numpy array, evaluation of the derivative of the LHS
 	'''
-	
+
 	LHSVals = ((alpha/4)*w)*cos(w) + (alpha/4 - 1)*sin(w) #default np array ops are element-wise
 	return LHSVals
 
@@ -68,7 +68,7 @@ def ProductCos(theta):
 		theta: 	(2,) float numpy array, value of the quasimomentum to evaluate the LHS at
 	Outputs:
 		RHSVals: (n,) float numpy array, evaluation of the RHS product-cos term
-	'''	
+	'''
 
 	thetaSum = theta.sum(axis=0) / 2. #(theta1+theta2)/2, for each column
 	thetaDiff = (theta[0] - theta[1]) / 2. #(theta1-theta2)/2, for each column
@@ -86,19 +86,19 @@ def EstimateBandStart(bandIndex, alpha, nPts=1000, precise=False):
 	Outputs:
 		bandStart: 	float, the estimated start point of the band I_bandIndex
 	'''
-	
+
 	if ((alpha<=2) and (bandIndex==1)):
 		#if alpha<=2 the first band is simply (0,pi), so return 0.
 		return 0.
-	
+
 	#otherwise, compute an estimate for the start of the band
 	wTestPts = linspace((bandIndex-1)*pi, bandIndex*pi, num=nPts, endpoint=True)
 	wTestPts = wTestPts[1:] #we know that (bandIndex-1)pi is part of the previous band, so ignore it
-	
+
 	LHSVals = LHS(wTestPts, alpha) #evaluate LHS
 	wBandLowest = wTestPts[np.abs(LHSVals)<=1][0] #any w corresponding to an absolute value of the LHS that's less than one is a part of this band. NOTE: np.abs(LHSVals)<=1 should give an array that is just False False... False True True True.... True
 	wOutBoundHighest = wTestPts[np.abs(LHSVals)>1][-1] #this is the greatest value of w we looked at that is not part of the band
-	
+
 	if precise:
 		#if true, we want a better estimate for the start of the band, use fsolve to get it
 		x0 = (wBandLowest + wOutBoundHighest)/2 #start point for fsolve
@@ -134,12 +134,12 @@ def BandIDoS(bandIndex, alpha, nPts=1000, **kwargs):
 		t1: 	(optional) (n,) float numpy array, custom points to use for discretising the 1st component of the quasimomentum
 		t2: 	(optional) (n,) float numpy array, custom points to use for discretising the 2ns component of the quasimomentum
 		visual: 	(optional) bool, if provided as True then the IDoS will be plotted before the function returns
-		kwargs will be passed to the following functions calls: 
+		kwargs will be passed to the following functions calls:
 			Visual_BandIDoS
 	OUTPUTS:
-		bIDoS: function, the IDoS for this band - see the function's docstring via help() for details.	
+		bIDoS: function, the IDoS for this band - see the function's docstring via help() for details.
 	'''
-	
+
 	#check if custom meshes for theta have been provided
 	if 't1' in kwargs.keys():
 		#custom theta1 range provided, use this
@@ -157,7 +157,7 @@ def BandIDoS(bandIndex, alpha, nPts=1000, **kwargs):
 		#use linspace for theta2, with nPts points
 		theta2 = linspace(-pi, pi, num=nPts, endpoint=True)
 		t2Pts = nPts
-		
+
 	#compute midpoint values for each component of the quasimomentum
 	t1Mids = (theta1[1:] + theta1[:-1]) / 2.
 	t2Mids = (theta2[1:] + theta2[:-1]) / 2.
@@ -182,7 +182,7 @@ def BandIDoS(bandIndex, alpha, nPts=1000, **kwargs):
 			f = lambda x: LHS(x, alpha) - RHSVal
 			wBandVals[i,j] = fsolve(f, w0, fprime=df)
 	print('Complete')
-	
+
 	#check that all the values that fsolve found are still in this band, otherwise we have a problem!
 	outOfBounds = (wBandVals<wStartEst) & (wBandVals>wEnd)
 	if outOfBounds.sum()!=0:
@@ -196,7 +196,7 @@ def BandIDoS(bandIndex, alpha, nPts=1000, **kwargs):
 					print('w: %.3f, t1: %.3f, t2: %.3f' % (wBandVals[i,j], t1Mids[i], t2Mids[j]))
 		print('Provided output: wBandVals, t1Mids, t2Mids, outOfBounds')
 		return wBandVals, t1Mids, t2Mids, outOfBounds
-	
+
 	#otherwise, we have found the values of w (in this band) that each QM-value corresponds to, now we just write a function that evaluates the IDoS for this band!
 	def bIDoS(x):
 		'''
@@ -206,7 +206,7 @@ def BandIDoS(bandIndex, alpha, nPts=1000, **kwargs):
 		OUTPUTS:
 			iDoS: (n,) float, values of the integrated density of states for this band
 		'''
-		
+
 		#this is just so that the function can be used as if it was vectorised
 		if np.isscalar(x):
 			iDoS = np.sum(wBandAreas[wBandVals<x])
@@ -216,7 +216,7 @@ def BandIDoS(bandIndex, alpha, nPts=1000, **kwargs):
 				iDoS[i] = np.sum(wBandAreas[wBandVals<x[i]])
 		return iDoS
 	#with the function created, we can now return it. But first we check if the user wanted to see a plot of this thing too
-	
+
 	if 'visual' in kwargs.keys():
 		if kwargs['visual']:
 			f = Visual_BandIDoS(bIDoS, bandIndex, bandBounds=np.asarray([wStartEst, wEnd]), **kwargs)
@@ -230,14 +230,14 @@ def BandDoS(bandIDoS, dx=1e-4, **kwargs):
 	INPUTS:
 		bandIDoS: 	lambda function, which takes in a single arugment x and returns the IDoS for a certain band
 		dx: 	(optional) float - default 1e-4, the step size to use in finite difference code
-		bandIndex: (optional) int - default 1, if plotting is 
+		bandIndex: (optional) int - default 1, if plotting is
 	OUTPUTS:
 		DoS: 	lambda function, takes a single argument x and returns the DoS for the same band as bandIDoS. See function docstring for further details.
-		
+
 	The argument dx needs to be given some thought - due to the way in which we construct the IDoS (by gridding the QM and testing the midpoint of the resulting cells in the mesh) the level of refinement in our QM mesh places a limit on the value of dx that we can use in estimating the derivative. A mesh size dt in QM-space will, through equation (2.7), result in some quantifiable expected change dw in the eigenvalue, but this is a jump and not a smooth transition in the eyes of the machine. As a result, IDoS is actually a series of tiny steps rather than a smooth line (it's just that the mesh size dt is small enough to make it look smooth) and hence DoS will display "spikes" if dx<dw (as IDoS is constant over intervals <dw in length).
 	As such, care should be taken when choosing dx, with this consideration in mind.
 	'''
-	
+
 	def bDoS(x):
 		'''
 		Given a value x>=0, returns the Density of States (DoS) for a single band, computed by finite differences on the corresponding Integrated Density of States
@@ -246,7 +246,7 @@ def BandDoS(bandIDoS, dx=1e-4, **kwargs):
 		OUTPUTS:
 			iDoS: (n,) float, values of the integrated density of states for this band
 		'''
-		
+
 		DoS = ( bandIDoS(x+dx) - bandIDoS(x) ) / dx
 		return DoS
 
@@ -267,7 +267,7 @@ def Visual_LHS(alpha, firstBand=1, lastBand=2, bandPts=1000, onePlot=True):
 		f: 	figure handle, figure displaying the spectral bands and LHS of (2.7), see figure 2.4 in the TFR.
 	Note: One can retrieve the x,y data from the figure handle f, see https://stackoverflow.com/questions/20130768/retrieve-xy-data-from-matplotlib-figure
 	'''
-	
+
 	if firstBand>lastBand:
 		raise ValueError('firstBand > lastBand (checked %d > %d) - aborting plot.' % (firstBand, lastBand))
 	elif firstBand<=0:
@@ -297,12 +297,12 @@ def Visual_LHS(alpha, firstBand=1, lastBand=2, bandPts=1000, onePlot=True):
 		ax[0].set_xlabel(r'$\frac{\omega}{\pi}$')
 		ax[0].set_ylabel(r'$\Xi$')
 		ax[1].set_xlabel(r'$\frac{\omega}{\pi}$')
-		ax[1].set_ylabel(r'Spectral Bands')		
+		ax[1].set_ylabel(r'Spectral Bands')
 		ax[0].plot(wPts/pi, LHSVals, 'r')
 		ax[0].plot(wPts/pi, np.ones_like(LHSVals, dtype=int), 'k')
 		ax[0].plot(wPts/pi, -1*np.ones_like(LHSVals, dtype=int), 'k')
 		ax[1].plot(wPts[specPts==0]/pi, specPts[specPts==0], 'bx')
-	
+
 	return f
 
 def Test_BandEstimatesVisual(bandIndex, alpha, nPts=1000):
@@ -315,14 +315,14 @@ def Test_BandEstimatesVisual(bandIndex, alpha, nPts=1000):
 	OUTPUTS:
 		f: 	figure, figure containing the visualisation
 	'''
-	
+
 	wTestPts = linspace((bandIndex-1)*pi, bandIndex*pi, num=5*nPts, endpoint=True) #extra points for precision
 	LHSVals = LHS(wTestPts, alpha) #evaluate LHS at all points in the interval
-	
+
 	#get estimates for the start of the band, both using the precise and imprecise methods
 	estBandStart = EstimateBandStart(bandIndex, alpha, nPts=nPts)
 	estBandStartPrecise = EstimateBandStart(bandIndex, alpha, nPts=nPts, precise=True)
-	
+
 	f, ax = plt.subplots()
 	ax.plot(wTestPts/pi, LHSVals, 'red')
 	ax.plot(wTestPts/pi, np.ones_like(wTestPts), 'black')
@@ -334,7 +334,7 @@ def Test_BandEstimatesVisual(bandIndex, alpha, nPts=1000):
 	ax.set_ylim(-1.5,1.5)
 	ax.set_xlabel(r'$\frac{\omega}{\pi}$')
 	ax.set_title(r'Estimate Start Points for band $I_{%d}$' %bandIndex)
-	
+
 	return f
 
 def Visual_BandIDoS(d, bandIndex, **kwargs):
@@ -349,7 +349,7 @@ def Visual_BandIDoS(d, bandIndex, **kwargs):
 	OUTPUTS:
 		f: 	figure, figure containing a plot of the IDoS
 	'''
-	
+
 	if 'nPts' in kwargs.keys():
 		nPts = kwargs['nPts']
 	else:
@@ -360,12 +360,12 @@ def Visual_BandIDoS(d, bandIndex, **kwargs):
 		plotBB = True
 	else:
 		plotBB = False
-	
+
 	if plotBB:
 		wBandPts = linspace(wStart, wEnd, num=nPts, endpoint=True)
 		wPts = np.concatenate((linspace((bandIndex-1)*pi, wStart, endpoint=False), wBandPts, linspace(wEnd, wEnd+pi/8)))  #add some padding around the band just to check we are constant outside it
 		bandIDoSVals = d(wPts)
-		
+
 		f, ax = plt.subplots()
 		ax.plot(wPts/pi, bandIDoSVals, 'red')
 		ax.plot(wStart/pi, d(wStart), 'xb')
@@ -376,12 +376,12 @@ def Visual_BandIDoS(d, bandIndex, **kwargs):
 	else:
 		wPts = linspace((bandIndex-1)*pi - pi/2, bandIndex*pi + pi/2, num=2*nPts, endpoint=True) #add some padding around the band just to check we are constant outside it
 		bandIDoSVals = d(wPts)
-		
+
 		f, ax = plt.subplots()
 		ax.plot(wPts/pi, bandIDoSVals, 'red')
 		ax.set_xlabel(r'$\frac{\omega}{\pi}$')
 		ax.set_ylabel(r'IDoS')
-		ax.set_title(r'Integrated Density of States for band $I_{%d}$' %bandIndex)		
+		ax.set_title(r'Integrated Density of States for band $I_{%d}$' %bandIndex)
 	return f
 
 def Visual_BandDoS(d, bandIndex, **kwargs):
@@ -396,7 +396,7 @@ def Visual_BandDoS(d, bandIndex, **kwargs):
 	OUTPUTS:
 		f: 	figure, figure containing a plot of the IDoS
 	'''
-	
+
 	if 'nPts' in kwargs.keys():
 		nPts = kwargs['nPts']
 	else:
@@ -407,12 +407,12 @@ def Visual_BandDoS(d, bandIndex, **kwargs):
 		plotBB = True
 	else:
 		plotBB = False
-	
+
 	if plotBB:
 		wBandPts = linspace(wStart, wEnd, num=nPts, endpoint=True)
 		wPts = np.concatenate((linspace((bandIndex-1)*pi, wStart, endpoint=False), wBandPts, linspace(wEnd, wEnd+pi/8)))  #add some padding around the band just to check we are constant outside it
 		bandDoSVals = d(wPts)
-		
+
 		f, ax = plt.subplots()
 		ax.plot(wPts/pi, bandDoSVals, 'red')
 		ax.plot(wStart/pi, d(wStart), 'xb')
@@ -423,12 +423,12 @@ def Visual_BandDoS(d, bandIndex, **kwargs):
 	else:
 		wPts = linspace((bandIndex-1)*pi - pi/2, bandIndex*pi + pi/2, num=2*nPts, endpoint=True) #add some padding around the band just to check we are constant outside it
 		bandDoSVals = d(wPts)
-		
+
 		f, ax = plt.subplots()
 		ax.plot(wPts/pi, bandDoSVals, 'red')
 		ax.set_xlabel(r'$\frac{\omega}{\pi}$')
 		ax.set_ylabel(r'DoS')
-		ax.set_title(r'Density of States for band $I_{%d}$' %bandIndex)		
+		ax.set_title(r'Density of States for band $I_{%d}$' %bandIndex)
 	return f
 
 def Visual_FullDoS(alpha, firstBand=1, lastBand=2, bandPts=1000, normIDoS=True, **kwargs):
@@ -449,14 +449,14 @@ def Visual_FullDoS(alpha, firstBand=1, lastBand=2, bandPts=1000, normIDoS=True, 
 		f: 	figure handle, figure displaying the DoS, IDoS, and spectral bands stacked atop each other
 	Note: One can retrieve the x,y data from the figure handle f, see https://stackoverflow.com/questions/20130768/retrieve-xy-data-from-matplotlib-figure
 	'''
-	
+
 	if firstBand>lastBand:
 		raise ValueError('firstBand > lastBand (checked %d > %d) - aborting plot.' % (firstBand, lastBand))
 	elif firstBand<=0:
 		raise ValueError('firstBand <=0 (got %d)' % (firstBand))
 	else:
 		nBands = lastBand-firstBand +1 #if equal, we still want to plot that one band
-	
+
 	#catch potential mesh-size problems
 	if ('nPts' in kwargs.keys()) and ('dx' in kwargs.keys()):
 		if kwargs['dx'] < 1./kwargs['nPts']:
@@ -493,7 +493,7 @@ def Visual_FullDoS(alpha, firstBand=1, lastBand=2, bandPts=1000, normIDoS=True, 
 		DoSVals += bandDoS(wPts)
 	if normIDoS:
 		#if normIDoS is true, we want to subtract (2pi)^2 from IDoS after each band (so the plot displays the relative increase of each band, and doesn't get a large y-axis)
-		iDoSVals -= ((2*pi)**2)*(firstBand-1) #starting at firstBand, there are firstBand-1 lower bands which are all adding an extra (2pi)^2 to the IDoS 
+		iDoSVals -= ((2*pi)**2)*(firstBand-1) #starting at firstBand, there are firstBand-1 lower bands which are all adding an extra (2pi)^2 to the IDoS
 		for band in range(firstBand, lastBand): #don't need to do the last band as it should have been normalised
 			#now for each band that we go through, we need to sucessively remove the extra (2pi)^2 contribution it gives
 			subtractArray = np.zeros_like(wPts, dtype=int)
@@ -509,5 +509,5 @@ def Visual_FullDoS(alpha, firstBand=1, lastBand=2, bandPts=1000, normIDoS=True, 
 	ax[2].plot(wPts/pi, DoSVals, 'b')
 	ax[2].set_ylabel(r'DoS')
 	ax[0].set_title(r'$\alpha=%.2f$' % (alpha))
-	
+
 	return f
